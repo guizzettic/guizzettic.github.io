a34103e480cc78f26afaa4f0154d479e27207d8c
Chris Guizzetti <guizzettic@gmail.com>
2024-05-15 08:57:09 -0700

    update testimonial card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import TestimonialCard from "./TestimonialCard";
-import sarah from "./assets/profile-thumbnail.png";
+import defaultAvatar from "./assets/profile-thumbnail.png";
 
 export interface IndividualCard {
   user: string;
   accountName: string;
   message: string;
-  avatar: string;
+  avatar?: string;
 }
 
 function App() {
@@ -16,7 +16,14 @@ function App() {
       accountName: "@sarahdole",
       message:
         "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
-      avatar: sarah,
+      avatar: defaultAvatar,
+    },
+    {
+      user: "John Doe",
+      accountName: "@johndoe",
+      message:
+        "This platform is fantastic! The user experience is top-notch, and the design resources are invaluable.",
+      avatar: defaultAvatar,
     },
   ];
 

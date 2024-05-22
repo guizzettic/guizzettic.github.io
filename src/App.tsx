3bdfac6ee2cae44b25340622de2a60e51b188a3e
Chris Guizzetti <guizzettic@gmail.com>
2024-05-21 17:05:39 -0700

    reset main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import "./App.css";
 import TestimonialCard from "./TestimonialCard";
+
 import defaultAvatar from "./assets/profile-thumbnail.png";
 
 export interface IndividualCard {
   user: string;
   accountName: string;
   message: string;
-  avatar?: string;
+  avatar: string;
 }
 
 function App() {
-  const testimonialCards: IndividualCard[] = [
-    {
-      user: "Sarah Dole",
-      accountName: "@sarahdole",
-      message:
-        "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
-      avatar: defaultAvatar,
-    },
-  ];
+  const testimonialCard: IndividualCard = {
+    user: "Sarah Dole",
+    accountName: "@sarahdole",
+    message:
+      "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+    avatar: defaultAvatar,
+  };
+  testimonialCard;
 
   return (
-    <>
-      {testimonialCards.map((card, index) => (
-        <TestimonialCard card={card} key={index} />
-      ))}
-    </>
+    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-br from-white to-gray-300">
+      <TestimonialCard user={testimonialCard} />
+    </div>
   );
 }
 
